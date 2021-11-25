b1f3158bc1b1b9e1553c453bf4bd28ce48556055
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-25 20:12:43 +0100

    header section completed

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="container">
-    <h3>This a header</h3>
+  <div>
+    <div class="hero-back hero-height">
+      <div class="h-full grid place-content-center">
+        <h1 class="text-5xl font-semibold text-gray-800">Best food in the town!</h1>
+        <button
+          class="bg-green-700 hover:bg-green-600 transition duration-300 rounded-full shadow py-2 px-3 my-5 font-semibold text-gray-50 w-2/4 mx-auto"
+        >More Foods</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,3 +20,12 @@ export default {
   }
 }
 </script>
+
+<style>
+.hero-back {
+  background-image: url(../assets/bannerbackground.png);
+  background-position: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
