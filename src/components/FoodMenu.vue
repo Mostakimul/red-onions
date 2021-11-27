b872ea37c72ba887e58c109c7c978fd24a1f476b
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-27 02:50:55 +0100

    lunch and dinner added with filter in home

--- src/components/FoodMenu.vue
+++ src/components/FoodMenu.vue
@@ -4,19 +4,21 @@
     <!-- menu selection -->
     <div class="flex justify-center items-center space-x-3">
       <button
-        type="button"
-        class="bg-green-600 hover:bg-green-700 py-1 px-5 rounded-full shadow font-medium text-gray-50"
+        @click="filterMealType('breakfast')"
+        class="bg-green-600 hover:bg-green-700 py-1 px-5 rounded-full shadow font-medium text-gray-50 cursor-pointer"
       >Breakfast</button>
       <button
-        class="bg-green-700 hover:bg-green-600 py-1 px-5 rounded-full shadow font-medium text-gray-50"
+        @click="filterMealType('lunch')"
+        class="bg-green-700 hover:bg-green-600 py-1 px-5 rounded-full shadow font-medium text-gray-50 cursor-pointer"
       >Lunch</button>
       <button
-        class="bg-green-600 hover:bg-green-700 py-1 px-5 rounded-full shadow font-medium text-gray-50"
+        @click="filterMealType('dinner')"
+        class="bg-green-600 hover:bg-green-700 py-1 px-5 rounded-full shadow font-medium text-gray-50 cursor-pointer"
       >Dinner</button>
     </div>
     <!-- Food render -->
     <section class="grid grid-cols-3 gap-3 py-5">
-      <div v-for="menu in homeMeal" :key="menu.id">
+      <div v-for="menu in foodMenus" :key="menu.id">
         <food-item :menu="menu"></food-item>
       </div>
     </section>
@@ -25,6 +27,7 @@
 
 <script>
 import { storeToRefs } from "pinia";
+import { onMounted, ref } from "vue";
 import { useMenu } from "../store";
 import FoodItem from "./FoodItem.vue"
 
@@ -33,9 +36,21 @@ export default {
   setup() {
     const allMenus = useMenu()
     const { menus } = storeToRefs(allMenus)
+    let foodMenus = ref(menus.value);
+
+    // filtering menus onClick
+    const filterMealType = (mealType) => {
+      const filteredMeal = menus.value.filter((menu) => menu.type === mealType)
+      foodMenus.value = filteredMeal
+    }
+
+    // Slicing the menu into 6
+    onMounted(() => {
+      foodMenus.value = foodMenus.value.slice(0, 6)
+    })
 
 
-    return { menus, homeMeal: allMenus.homeMeal };
+    return { menus, foodMenus, filterMealType };
   },
 
 }
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,18 @@ import breakfast3 from '../assets/Breakfast/breakfast3.png';
 import breakfast4 from '../assets/Breakfast/breakfast4.png';
 import breakfast5 from '../assets/Breakfast/breakfast5.png';
 import breakfast6 from '../assets/Breakfast/breakfast6.png';
+import dinner1 from '../assets/Dinner/dinner1.png';
+import dinner2 from '../assets/Dinner/dinner2.png';
+import dinner3 from '../assets/Dinner/dinner3.png';
+import dinner4 from '../assets/Dinner/dinner4.png';
+import dinner5 from '../assets/Dinner/dinner5.png';
+import dinner6 from '../assets/Dinner/dinner6.png';
+import lunch1 from '../assets/lunch/lunch1.png';
+import lunch2 from '../assets/lunch/lunch2.png';
+import lunch3 from '../assets/lunch/lunch3.png';
+import lunch4 from '../assets/lunch/lunch4.png';
+import lunch5 from '../assets/lunch/lunch5.png';
+import lunch6 from '../assets/lunch/lunch6.png';
 
 export const useMenu = defineStore('menu', {
   state: () => {
@@ -71,16 +83,106 @@ export const useMenu = defineStore('menu', {
           price: 4.99,
           details:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga labore repellendus earum eveniet impedit at assumenda sunt saepe nobis!',
-          image: breakfast6,
+          image: dinner1,
         },
         {
           id: 8,
           name: 'Healthy Meal Plan',
-          type: 'lunch',
-          price: 4.99,
+          type: 'dinner',
+          price: 8.99,
           details:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga labore repellendus earum eveniet impedit at assumenda sunt saepe nobis!',
-          image: breakfast6,
+          image: dinner2,
+        },
+        {
+          id: 9,
+          name: 'Healthy Meal Plan',
+          type: 'dinner',
+          price: 8.99,
+          details:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga labore repellendus earum eveniet impedit at assumenda sunt saepe nobis!',
+          image: dinner3,
+        },
+        {
+          id: 10,
+          name: 'Healthy Meal Plan',
+          type: 'dinner',
+          price: 8.99,
+          details:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga labore repellendus earum eveniet impedit at assumenda sunt saepe nobis!',
+          image: dinner4,
+        },
+        {
+          id: 11,
+          name: 'Healthy Meal Plan',
+          type: 'dinner',
+          price: 8.99,
+          details:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga labore repellendus earum eveniet impedit at assumenda sunt saepe nobis!',
+          image: dinner5,
+        },
+        {
+          id: 12,
+          name: 'Healthy Meal Plan',
+          type: 'dinner',
+          price: 8.99,
+          details:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga labore repellendus earum eveniet impedit at assumenda sunt saepe nobis!',
+          image: dinner6,
+        },
+        {
+          id: 13,
+          name: 'Healthy Meal Plan',
+          type: 'lunch',
+          price: 8.99,
+          details:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga labore repellendus earum eveniet impedit at assumenda sunt saepe nobis!',
+          image: lunch1,
+        },
+        {
+          id: 14,
+          name: 'Healthy Meal Plan',
+          type: 'lunch',
+          price: 8.99,
+          details:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga labore repellendus earum eveniet impedit at assumenda sunt saepe nobis!',
+          image: lunch2,
+        },
+        {
+          id: 15,
+          name: 'Healthy Meal Plan',
+          type: 'lunch',
+          price: 8.99,
+          details:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga labore repellendus earum eveniet impedit at assumenda sunt saepe nobis!',
+          image: lunch3,
+        },
+        {
+          id: 16,
+          name: 'Healthy Meal Plan',
+          type: 'lunch',
+          price: 8.99,
+          details:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga labore repellendus earum eveniet impedit at assumenda sunt saepe nobis!',
+          image: lunch4,
+        },
+        {
+          id: 17,
+          name: 'Healthy Meal Plan',
+          type: 'lunch',
+          price: 8.99,
+          details:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga labore repellendus earum eveniet impedit at assumenda sunt saepe nobis!',
+          image: lunch5,
+        },
+        {
+          id: 18,
+          name: 'Healthy Meal Plan',
+          type: 'lunch',
+          price: 8.99,
+          details:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fuga labore repellendus earum eveniet impedit at assumenda sunt saepe nobis!',
+          image: lunch6,
         },
       ],
       counter: 0,
