600121b63922a0932f3c967ce36296ff354323c4
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-28 01:20:58 +0100

    login form added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    <h2>This is Login Page</h2>
+  <div class="container">
+    <div>
+      <img src="../assets/logo2.png" alt="logo" class="w-1/4 mx-auto" />
+    </div>
+    <div class="my-5">
+      <form class="w-2/4 bg-gray-200 py-5 rounded shadow text-center mx-auto">
+        <input
+          id="name"
+          type="text"
+          placeholder="Name"
+          class="bg-gray-100 py-2 px-2 my-1.5 w-3/5 rounded"
+        />
+        <input
+          id="email"
+          type="email"
+          placeholder="Email"
+          class="bg-gray-100 py-2 px-2 my-1.5 w-3/5 rounded"
+        />
+        <input
+          id="password"
+          type="password"
+          placeholder="Password"
+          class="bg-gray-100 py-2 px-2 my-1.5 w-3/5 rounded"
+        />
+
+        <button
+          class="bg-red-600 hover:bg-red-700 text-gray-50 block w-3/5 text-center mx-auto py-2 my-1.5 rounded shadow"
+        >Sign in</button>
+      </form>
+    </div>
   </div>
 </template>
 
