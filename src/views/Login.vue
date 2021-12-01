dddfecd32ec1df770626ecfe74c8ef24abeef9f3
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-12-01 23:20:51 +0100

    login and register form updated

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,14 +3,8 @@
     <div>
       <img src="../assets/logo2.png" alt="logo" class="w-1/4 mx-auto" />
     </div>
-    <div class="my-5">
-      <form class="w-2/4 bg-gray-200 py-5 rounded shadow text-center mx-auto">
-        <input
-          id="name"
-          type="text"
-          placeholder="Name"
-          class="bg-gray-100 py-2 px-2 my-1.5 w-3/5 rounded"
-        />
+    <div class="my-5 w-2/4 bg-gray-200 py-5 rounded shadow text-center mx-auto">
+      <form>
         <input
           id="email"
           type="email"
@@ -28,6 +22,10 @@
           class="bg-red-600 hover:bg-red-700 text-gray-50 block w-3/5 text-center mx-auto py-2 my-1.5 rounded shadow"
         >Sign in</button>
       </form>
+      <p>
+        Don't have an account?
+        <router-link :to="{ name: 'Register' }" class="text-green-600">register here</router-link>
+      </p>
     </div>
   </div>
 </template>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,39 @@
 <template>
-  <div>
-    <h2>This is register page!</h2>
+  <div class="container">
+    <div>
+      <img src="../assets/logo2.png" alt="logo" class="w-1/4 mx-auto" />
+    </div>
+    <div class="my-5 w-2/4 bg-gray-200 py-5 rounded shadow text-center mx-auto">
+      <form>
+        <input
+          id="name"
+          type="text"
+          placeholder="Name"
+          class="bg-gray-100 py-2 px-2 my-1.5 w-3/5 rounded"
+        />
+        <input
+          id="email"
+          type="email"
+          placeholder="Email"
+          class="bg-gray-100 py-2 px-2 my-1.5 w-3/5 rounded"
+        />
+        <input
+          id="password"
+          type="password"
+          placeholder="Password"
+          class="bg-gray-100 py-2 px-2 my-1.5 w-3/5 rounded"
+        />
+
+        <button
+          class="bg-red-600 hover:bg-red-700 text-gray-50 block w-3/5 text-center mx-auto py-2 my-1.5 rounded shadow"
+        >Register</button>
+      </form>
+
+      <p>
+        Already have an account?
+        <router-link :to="{ name: 'Login' }" class="text-green-600">login</router-link>
+      </p>
+    </div>
   </div>
 </template>
 
